e0ef21a0c3a1caffaab667349e23388e5901ded8
rsuubinn <52683129+rsuubinn@users.noreply.github.com>
2023-03-31 23:55:01 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1284 ms, Memory: 163160 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,13 @@
+let fs = require('fs');
+let input = fs.readFileSync('dev/stdin').toString().split('\n');
+let testCase = Number(input[0]);
+let result = '';
+
+for(let i = 1; i <= testCase; i++){
+    let data = input[i].split(' ');
+    let a = Number(data[0]);
+    let b = Number(data[1]);
+    result += a + b + '\n';
+}
+
+console.log(result);
