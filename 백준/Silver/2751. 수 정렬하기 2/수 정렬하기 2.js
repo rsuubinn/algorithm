c5305c4af1aa14bcf10a27a28f861468895a0950
rsuubinn <52683129+rsuubinn@users.noreply.github.com>
2023-04-05 23:20:15 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2256 ms, Memory: 189432 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+const N = input.shift();
+let results = '';
+input.sort((a, b) => a - b).forEach(num => (results += `${num}\n`));
+
+console.log(results);
