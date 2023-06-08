7844aff4744950bc93edee7776b4a81911c6d10d
rsuubinn <52683129+rsuubinn@users.noreply.github.com>
2023-06-09 00:26:08 +0900

    [Silver V] Title: 덩치, Time: 124 ms, Memory: 9652 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.js
+++ 백준/Silver/7568. 덩치/덩치.js
@@ -1,20 +1,20 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
+let testCase = Number(input[0]);
 let ranks = [];
 
-for (let i = 1; i <= Number(input[0]); i++) {
-  let currentPerson = input[i].split(' ');
-  let currentWeight = Number(currentPerson[0]);
-  let currentHeight = Number(currentPerson[1]);
-  let count = 0;
+for(let i = 1; i <= testCase; i++) {
+    let currentPerson = input[i].split(' ');
+    let weight = Number(currentPerson[0]);
+    let height =  Number(currentPerson[1]);
+    let count = 0;
 
-  for (let j = 1; j <= Number(input[0]); j++) {
-    if (currentWeight < Number(input[j].split(' ')[0]) && currentHeight < Number(input[j].split(' ')[1])) {
-      count++;
+    for(let j = 1; j <= testCase; j++) {
+        if(weight < Number(input[j].split(' ')[0]) && height < Number(input[j].split(' ')[1])) {
+            count++;
+        }
     }
-  }
-
-  ranks.push(count + 1);
+    ranks.push(count + 1);
 }
 
 const result = ranks.join(' ');
