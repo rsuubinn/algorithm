9d5b4cd2bad994386b48efde8745f96f062a37b8
rsuubinn <52683129+rsuubinn@users.noreply.github.com>
2023-06-22 13:33:16 +0900

    [Silver IV] Title: 문서 검색, Time: 172 ms, Memory: 9728 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.js
+++ 백준/Silver/1543. 문서 검색/문서 검색.js
@@ -1,29 +1,27 @@
 /*
-    1. 검색할 키워드 길이만큼 주어진 문장 앞 부분 같은지 확인(for문)
-    1-1. 같으면 count++, splice를 이용하여 같은 부분 잘라내기
-    1-2. 같지 않으면 shift()를 이용하며 주어진 문장 앞 잘라내기
+    1. 검색할 키워드 길이만큼 주어진 문장 앞 부분 같은지 확인(for문, count 변수)
+    1-1. 같으면(count 크기와 키워드 길이가 같을 때) splice를 이용하여 같은 부분 잘라내기, answer++
+    1-2. 같지 않으면 shift()를 이용하며 sentence 맨 앞 한글자 버리기
+    2. answer 출력
 */
-
-
 
 let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let sentance = input[0].split('');
+let sentence = input[0].split('');
 const keyword = input[1];
 let answer = 0;
 
-while (sentance.length) {
+while (sentence.length) {
     let count = 0;
     for (let i = 0; i < keyword.length; i++) {
-        if (sentance[i] === keyword[i]) count += 1;
+        if (sentence[i] === keyword[i]) count += 1;
     }
-    if (count === keyword.length) { // 문자랑 키워드 같음
+    if (count === keyword.length) { 
         answer += 1;
-        sentance.splice(0, keyword.length);
+        sentence.splice(0, keyword.length);
 
     } else {
-        sentance.shift();
+        sentence.shift();
     }
 }
 console.log(answer);
-
