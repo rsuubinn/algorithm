e0483280e18c6acb13f6ace7e0e00e4f2d450fc9
rsuubinn <52683129+rsuubinn@users.noreply.github.com>
2023-06-23 15:32:20 +0900

    [Bronze I] Title: 비밀 이메일, Time: 160 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2999. 비밀 이메일/비밀 이메일.js
+++ 백준/Bronze/2999. 비밀 이메일/비밀 이메일.js
@@ -5,15 +5,40 @@
     이제 메시지를 행렬에 옮긴다.
 
     1. input 길이를 이용해서 R, C 구한다.
-    1-1. 1부터 input 
+    1-1 에라토스테네스의 체를 이용하여 input.length의 제곱근 값을 시작으로 R을 찾는다.
+    2. R이 1일 경우 input을 그냥 출력하면 된다.
+    3. R이 1이 아닐 경우 input 값을 2차원 배열로 저장
+    ex)
+    R:2, C:3
+    k o  [0][0]  [1][0]
+    a s  [0][1]  [1][1]
+    k i  [0][2]  [1][2]
+    4. 문제 출력 형식대로 answer 배열에 값을 저장한 후 answer 배열 출력
 
 */
 
-const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const input = require('fs').readFileSync('input.txt').toString().trim();
 
 let R = 0;
 let C = 0;
 
+
+/*
+내가 처음 생각했던 풀이과정
+if ((input.length / 2) * (input.length / 2) === input.length) {
+    R = input.length;
+    C = input.length;
+} else {
+    let arr = [];
+    for (let i = 1; i < input.length / 2; i++) {
+        if (input.length % i === 0) arr.push(i);
+    }
+    R = Math.max(...arr);
+    C = input.length / R;
+}
+*/
+
+// 다른 풀이 참고한 부분
 for (let i = Math.floor(Math.sqrt(input.length)); i >= 1; i--) {
     if (input.length % i === 0) {
         R = i;
@@ -45,12 +70,3 @@ else {
     }
     console.log(answer.join(''));
 }
-
-
-
-// 1 6
-// R:2, C:3
-
-// k o  [0][0]  [1][0]
-// a s  [0][1]  [1][1]
-// k i  [0][2]  [1][2]
